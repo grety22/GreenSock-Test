900e3547bc91924d0980a8663e5e3bf55cfe96d3
Gretel Martinez <gretelmn@gmail.com>
2018-07-25 00:26:46 -0400

    just backimg

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+var position = 50;
 
+var book = document.getElementById("booki");
+book.addEventListener("mouseover", function() { 
+    TweenMax.to(".book", 2, {left:position});    
+});
+book.addEventListener("mouseout", function() { 
+    TweenMax.to(".book", 0.5, {left:position-50});    
+});
+
+
+alert('algo');
